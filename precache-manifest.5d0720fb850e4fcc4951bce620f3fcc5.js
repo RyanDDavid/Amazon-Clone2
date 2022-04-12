79ab486ba032cda86cc1d6a5459ba26731ca0213
jhovany <jhovany.escob@gmail.com>
2022-04-12 17:37:39 -0500

    Updates

---
+++ precache-manifest.5d0720fb850e4fcc4951bce620f3fcc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812531053e4e471f846a62532a792f10",
+    "url": "/Amazon-Clone2/index.html"
+  },
+  {
+    "revision": "e09c03a115fbd8fa1808",
+    "url": "/Amazon-Clone2/static/css/main.0c8489f7.chunk.css"
+  },
+  {
+    "revision": "976ff2b5467fa1588da4",
+    "url": "/Amazon-Clone2/static/js/2.8a0a0383.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/Amazon-Clone2/static/js/2.8a0a0383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09c03a115fbd8fa1808",
+    "url": "/Amazon-Clone2/static/js/main.a727e6d8.chunk.js"
+  },
+  {
+    "revision": "dc6731724a86e0fee8ec",
+    "url": "/Amazon-Clone2/static/js/runtime-main.2f13a8c5.js"
+  }
+]);
